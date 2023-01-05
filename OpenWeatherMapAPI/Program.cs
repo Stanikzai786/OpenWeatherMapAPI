5cd26498b211ea8989a49de10b4d370b8591fd86
Ahmad Stanikzai <Shekibstanikzai.2016@gmail.com>
2023-01-05 09:21:48 -0800

    Done

---
+++ OpenWeatherMapAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var APIKey = "cb6db26af206690d6cc36f3b77565f32";
+            var cityName = "Fremont";
+            var endPointURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIKey}";
+            var client = new HttpClient();
+            var response = client.GetStringAsync(endPointURL).Result;
+            var weatherObject = JObject.Parse(response);
+
+            Console.WriteLine($"{weatherObject}");
+
+        }
+    }
+}
